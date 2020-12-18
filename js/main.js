97711795e5af75ded5833fb23c173d5dd4479570
Blitz-coder <blitz-coder@narod.ru>
2020-12-19 03:35:02 +0700

    create: history section

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
   var menuButton = document.querySelector(".navbar__menu-burger");
   menuButton.addEventListener('click', function () {
     document.querySelector(".navbar__mobile").classList.toggle("navbar__mobile--visible");
-    console.log("клик по бургеру");
   });
 
   var menuButton = document.querySelector(".navbar__menu-burger--close");
@@ -24,6 +23,33 @@ $(document).ready(function () {
       clickable: true,
     },
 
+  });
+
+  var historySlider = new Swiper('.history-slider', {
+    loopedSlides: 0,
+    loop: false,
+    // slidesPerView: 0,
+    spaceBetween: 0,
+    // slidesPerGroup: 0,
+    // slidesPerGroupSkip: 1,
+    slidesPerColumn: 2,
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 576px
+      576: {
+        loopedSlides: 2,
+        loop: true,
+        slidesPerView: 2,
+        spaceBetween: 24,
+        slidesPerGroup: 2,
+        slidesPerColumn: 1,
+      },
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.history-arrows__next',
+      prevEl: '.history-arrows__prev',
+    },
   });
 
 
